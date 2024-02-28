6b4c886a75424985d8614a66e3bba977f8f3e4f9
juadlopezcas <jlopezc1@jhu.edu>
2024-02-28 14:54:02 -0500

    scripts folder

--- stars.py
+++ stars.py
@@ -6,13 +6,15 @@ import pymanopt.manifolds as manifolds
 import pymanopt.optimizers as optimizers
 from pymanopt.tools.diagnostics import check_gradient
 from data import DataGenerator
+from scripts.plots import plot_gaussians_with_axes
 
 #SUPPORTED_BACKENDS = ("autograd", "numpy", "pytorch")
-manifold = manifolds.SpecialOrthogonalGroup(3)
+manifold = manifolds.SpecialOrthogonalGroup(2)
 @manopt.function.autograd(manifold)
 def cost(basis_matrix):
+    np.random.seed(4)
     dist = []
-    gen_dat = DataGenerator(1000, 3)
+    gen_dat = DataGenerator(1000, 2)
     points = gen_dat.cross_data()
     for point in points:
         closest_axis_index = np.argmax(np.abs(np.dot(point, basis_matrix)))
@@ -26,6 +28,11 @@ problem = manopt.Problem(manifold=manifold, cost=cost)
 
 # Example usage
 if __name__ == "__main__":
+    np.random.seed(4)
+    gen_dat = DataGenerator(1000, 2)
+    points = gen_dat.cross_data()
     solver = optimizers.SteepestDescent(verbosity=1)
     x_opt = solver.run(problem).point
     print(x_opt)
+    plot_gaussians_with_axes(x_opt, points)
+    
